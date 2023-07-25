a4128b2ca3ce4059c11ea9c23559df9fc687d6a2
Jeff Uchitjil <git@k9kld.org>
2023-07-24 19:45:00 -0500

    Enhance to support multiple wsjtx clients
    * Incorporate changes in the WsjtxClient library to allow for multiple clients on different ports.
    * Enhance logging configuration to support timestamps on console logs.

---
+++ WsjtxGps/ConfigServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+using WsjtxClient.Provider;
+using WsjtxGps.Service;
+
+namespace WsjtxGps;
+
+public static class ConfigServiceCollectionExtensions
+{
+    public static IServiceCollection ConfigureServicesFromConfig(this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddSingleton<IGpsDevice, KenwoodRadio>();
+        services.AddHostedService<IGpsDevice>(provider => provider.GetRequiredService<IGpsDevice>());
+
+        List<Listener> listeners = new List<Listener>();
+        config.GetSection("Wsjtx:Listeners").Bind(listeners);
+
+        services.AddTransient<IWsjtxClient, WsjtxClient.Provider.WsjtxClient>();
+        
+        
+        foreach (var listener in listeners)
+        {
+            Console.Out.WriteLine($"Adding listener on port {listener.Port}");
+            services.AddSingleton<IHostedService>(x => new WsjtxDataProvider(
+                x.GetRequiredService<ILogger<WsjtxDataProvider>>(),
+                x.GetRequiredService<IWsjtxClient>(),
+                listener));
+        }
+        
+        
+
+        return services;
+    }
+}
--- WsjtxGps/Program.cs
+++ WsjtxGps/Program.cs
@@ -1,17 +1,8 @@
-using WsjtxClient.Provider;
 using WsjtxGps;
-using WsjtxGps.Service;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
-    {
-        services.AddSingleton<IGpsDevice, KenwoodRadio>();
-        services.AddSingleton<IWsjtxClient, WsjtxClient.Provider.WsjtxClient>();
-        services.AddHostedService<IGpsDevice>(provider => provider.GetRequiredService<IGpsDevice>());
-        services.AddSingleton<IWsjtxDataProvider, WsjtxDataProvider>();
-        services.AddHostedService<IWsjtxDataProvider>(provider => provider.GetRequiredService<IWsjtxDataProvider>());
-        services.AddHostedService<Worker>();
-    })
-    .Build();
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.ConfigureServicesFromConfig(builder.Configuration);
+IHost host = builder.Build();
 
 host.Run();
